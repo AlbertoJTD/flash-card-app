5874fa6b2a42321d8cc954e42205fca455920806
AlbertoJTD <albertojtd@outlook.com>
2023-04-19 11:17:28 -0600

    Create CSV file for unknown words

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- GET A NEW WORD ------------------------------- #
 data = pandas.read_csv('data/french_words.csv')
-data = data.to_dict(orient='records')
+words_to_learn = data.to_dict(orient='records')
 current_card = {}
 
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(data)
+    current_card = random.choice(words_to_learn)
     canvas.itemconfig(card_title, text=list(current_card.keys())[0], fill='black')
     canvas.itemconfig(card_word, text=current_card['French'], fill='black')
     window.after(3000, flip_card)
@@ -26,6 +26,11 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card['English'], fill='white')
     canvas.itemconfig(background, image=card_back_image)
 
+def is_known():
+    words_to_learn.remove(current_card)
+    unknown_words = pandas.DataFrame(words_to_learn)
+    unknown_words.to_csv('words_to_learn.csv')
+    next_card()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -51,7 +56,7 @@ known_button= Button(image=check_mark_image, highlightthickness=0, bg=BACKGROUND
 known_button.grid(row=1, column=1)
 
 cross_mark_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_mark_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief="flat", command=next_card)
+unknown_button = Button(image=cross_mark_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief="flat", command=is_known)
 unknown_button.grid(row=1, column=0)
 
 next_card()
