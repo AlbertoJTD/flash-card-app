452614478282a4a097d745f33e40b1d8ae764815
AlbertoJTD <albertojtd@outlook.com>
2023-04-19 11:36:00 -0600

    Add file exception handling

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- GET A NEW WORD ------------------------------- #
-data = pandas.read_csv('data/french_words.csv')
-words_to_learn = data.to_dict(orient='records')
+try:
+    data = pandas.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    data = pandas.read_csv('data/french_words.csv')
+finally:
+    words_to_learn = data.to_dict(orient='records')
+
 current_card = {}
 
 def next_card():
@@ -16,8 +21,8 @@ def next_card():
     current_card = random.choice(words_to_learn)
     canvas.itemconfig(card_title, text=list(current_card.keys())[0], fill='black')
     canvas.itemconfig(card_word, text=current_card['French'], fill='black')
-    window.after(3000, flip_card)
     canvas.itemconfig(background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
 
 
 # ---------------------------- FLIP THE CARD ------------------------------- #
@@ -29,14 +34,14 @@ def flip_card():
 def is_known():
     words_to_learn.remove(current_card)
     unknown_words = pandas.DataFrame(words_to_learn)
-    unknown_words.to_csv('words_to_learn.csv')
+    unknown_words.to_csv('data/words_to_learn.csv', index=False)
     next_card()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-flip_timer = window.after(3000, flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 # Add image to the background
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -52,11 +57,11 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 check_mark_image = PhotoImage(file="images/right.png")
-known_button= Button(image=check_mark_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief="flat", command=next_card)
+known_button= Button(image=check_mark_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief="flat", command=is_known)
 known_button.grid(row=1, column=1)
 
 cross_mark_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_mark_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief="flat", command=is_known)
+unknown_button = Button(image=cross_mark_image, highlightthickness=0, bg=BACKGROUND_COLOR, relief="flat", command=next_card)
 unknown_button.grid(row=1, column=0)
 
 next_card()
